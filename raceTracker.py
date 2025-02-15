08048485d19e92f770e3c606e1db91c671cc2a5a
Oranjoyer <oranjoyer@gmail.com>
2025-02-15 15:12:36 -0500

    Made Template Comparison Module

--- raceTracker.py
+++ raceTracker.py
@@ -1,15 +1,21 @@
 import time
 import json
 
+# Sends Log Message with 'RaceTracker' source
+def sendMessage(type,message):
+    logManager.sendMessage(type, "RaceTracker", message)
+
 class Track:
-    def __init__(name,trackType,strategy):
+    def __init__(name,trackType,strategy,bestStartSpot):
         self.name = name
         self.trackType = trackType
         self.strategy = strategy
+        self.bestStartSpot = bestStartSpot
 class eventDetails:
-    def __init__(self,timeOccur, raceJSON, description):
-        self.timeOccur = timeOccur
-        self.conditions = raceJSON
+    def __init__(self,timeOccur, raceDict, description):
+        self.timeOccur = time.ctime(raceDict["endTime"])
+        self.timeElapsed = raceDict["endTime"]-raceDict["startTime"]
+        self.conditions = raceDict
         self.description = description
 
 class IndivRace:
@@ -39,9 +45,21 @@ class JointRace:
         races = [race for race in raceInstances]
         self.startTime = min([race.startTime for race in races])
         self.endTime = max([race.endTime for race in races])
+        self.track = raceInstances[0].track
+
+        # Input Validation
+        for race in raceInstances:
+            if(race.track != self.track):
+                sendMessage("Error","\'JointRace\' creation with races that have different track. THIS SHOULD NEVER HAPPEN")
+
+
+        self.averagePlace 
 
 
     def getPlayer(self, name):
         for race in races:
             if(name == race.player.name):
-                sendMessage()
+                sendMessage("Info",f"Player {name} found in race on track \'{self.track}\' that took place on {time.ctime(self.startTime)} and lasted {self.endTime-self.startTime} seconds")
+                return race.player 
+        sendMessage("Warning",f"Player {name} not found in race on track \'{self.track}\' that took place on {time.ctime(self.startTime)} and lasted {self.endTime-self.startTime} seconds")
+
